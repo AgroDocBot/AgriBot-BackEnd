154f1ca6c88e40127ed90ea35c36f69de4409c14
IvayloKartev <ivaylokartev@gmail.com>
2025-01-19 05:50:05 -0500

    Implemented Plants Controller and Service

--- src/plants/plants.controller.ts
+++ src/plants/plants.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
+import { PlantsService } from './plants.service';
 
 @Controller('plants')
-export class PlantsController {}
+export class PlantsController {
+  constructor(private readonly plantService: PlantsService) {}
+
+  @Post('healthy/add')
+  async addHealthyPlant(@Body() body: { latitude: number; longitude: number; crop: string; measurementId: number }) {
+    return await this.plantService.addHealthyPlant(body);
+  }
+
+  @Post('diseased/add')
+  async addDiseasedPlant(@Body() body: { latitude: number; longitude: number; crop: string; disease: string; measurementId: number }) {
+    return await this.plantService.addDiseasedPlant(body);
+  }
+
+  @Get('healthy/measurement/:measurementId')
+  async getHealthyPlantsByMeasurement(@Param('measurementId') measurementId: number) {
+    return await this.plantService.getHealthyPlants(Number(measurementId));
+  }
+
+  @Get('diseased/measurement/:measurementId')
+  async getDiseasedPlantsByMeasurement(@Param('measurementId') measurementId: number) {
+    return await this.plantService.getDiseasedPlants(Number(measurementId));
+  }
+
+  @Delete('healthy/:id')
+  async deleteHealthyPlant(@Param('id') id: number) {
+    return await this.plantService.deleteHealthyPlant(Number(id));
+  }
+
+  @Delete('diseased/:id')
+  async deleteDiseasedPlant(@Param('id') id: number) {
+    return await this.plantService.deleteDiseasedPlant(Number(id));
+  }
+}
--- src/plants/plants.service.ts
+++ src/plants/plants.service.ts
@@ -1,4 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class PlantsService {}
+export class PlantsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async addHealthyPlant(data: {
+    latitude: number;
+    longitude: number;
+    crop: string;
+    measurementId: number;
+  }) {
+    return await this.prisma.healthyPlant.create({
+      data,
+    });
+  }
+
+  async addDiseasedPlant(data: {
+    latitude: number;
+    longitude: number;
+    crop: string;
+    disease: string;
+    measurementId: number;
+  }) {
+    return await this.prisma.diseasedPlant.create({
+      data,
+    });
+  }
+
+  async getHealthyPlants(measurementId : number) {
+    return await this.prisma.healthyPlant.findMany({
+        where: {
+            measurementId
+        }
+    });
+  }
+
+  async getDiseasedPlants(measurementId : number) {
+    return await this.prisma.diseasedPlant.findMany({
+        where: {
+            measurementId
+        }
+    });
+  }
+
+  async deleteHealthyPlant(id: number) {
+    return await this.prisma.healthyPlant.delete({
+      where: { id },
+    });
+  }
+
+  async deleteDiseasedPlant(id: number) {
+    return await this.prisma.diseasedPlant.delete({
+      where: { id },
+    });
+  }
+}
